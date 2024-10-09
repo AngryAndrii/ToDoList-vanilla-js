22d94ba9ee7d361c1c4aa94ce5641cf083b88125
Andrii Chychkan <mywork1728@gmail.com>
2024-10-09 16:39:42 +0300

    added add block

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-console.log("hello");
+const addButton = document.querySelector("#add-button");
+const addBlock = document.querySelector(".add-block");
+
+addButton.addEventListener("click", (ev) => {
+  addBlock.classList.toggle("none")
+});
