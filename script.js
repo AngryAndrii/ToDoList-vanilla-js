3ec0ac73f426e95ca034a7e610aff3816ae401e9
Andrii Chychkan <mywork1728@gmail.com>
2024-10-10 16:30:11 +0300

    Dev (#4)
    * added html layout
    
    * added transition and some styles
    
    * added add block
    
    * added add template func and styles for buttons
    
    ---------
    
    Co-authored-by: Andrii Chychkan <mywork1728@gmail>

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
+import { addCard } from "./scripts/card.js";
+
 const addButton = document.querySelector("#add-button");
 const addBlock = document.querySelector(".add-block");
 
+const returnButton = document.querySelector("#return");
+
+const saveButton = document.querySelector("#save");
+
+const newName = document.querySelector("#new-name");
+const newDesc = document.querySelector("#new-descr");
+
 addButton.addEventListener("click", (ev) => {
-  addBlock.classList.toggle("none")
+  addBlock.classList.remove("none");
+  addButton.classList.add("none");
+});
+
+returnButton.addEventListener("click", (ev) => {
+  addBlock.classList.add("none");
+  addButton.classList.remove("none");
+});
+
+const getDataFromNew = () => {};
+
+saveButton.addEventListener("click", (ev) => {
+  const name = newName.value;
+  const descr = newDesc.value;
+  addCard(name, descr);
 });
