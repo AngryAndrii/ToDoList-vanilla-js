2db778590bba620ad9a30c1c5ef246de9b32d94d
Andrii Chychkan <mywork1728@gmail.com>
2024-10-10 16:23:29 +0300

    added add template func and styles for buttons

---
+++ scripts/card.js
@@ -0,0 +1,27 @@
+const addBlock = document.querySelector(".add-block");
+
+export const addCard = (name, descr) => {
+  const tamplate = `<div class="card">
+        <div class="corner-icons-container">
+          <svg class="icon">
+            <use href="./icons/sprite.svg#edit_icon"></use>
+          </svg>
+          <svg class="icon">
+            <use href="./icons/sprite.svg#trash_icon"></use>
+          </svg>
+        </div>
+        <span class="tag">домашні справи</span>
+        <h2 class="name">${name} </h2>
+        <p class="description">
+        ${descr}
+        </p>
+        <button class="done-button">
+          <svg class="icon clipboard-icon">
+            <use href="./icons/sprite.svg#clipboard_icon"></use>
+          </svg>
+          Завершити
+        </button>
+      </div>`;
+
+  addBlock.insertAdjacentHTML("beforebegin", tamplate);
+};
